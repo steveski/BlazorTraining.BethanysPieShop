cacb372d4ad8d791165703613e636c5301c5711a
steve <steve@steve-york.com>
2020-06-14 17:54:53 +1000

    Implemented AddEmployeeDialog component and using it from a + button in the bottom right of the overview page.

---
+++ BethanysPieShop.Server/Components/AddEmployeeDialogBase.cs
@@ -0,0 +1,58 @@
+﻿namespace BethanysPieShop.Server.Components
+{
+    using System;
+    using System.Threading.Tasks;
+    using BethanysPieShopHRM.Shared;
+    using Microsoft.AspNetCore.Components;
+    using Services;
+
+    public class AddEmployeeDialogBase : ComponentBase
+    {
+        public Employee Employee { get; set; } = new Employee
+        {
+            CountryId = 1,
+            JobCategoryId = 1,
+            BirthDate = DateTime.Now,
+            JoinedDate = DateTime.Now
+        };
+
+        [Inject] public IEmployeeDataService EmployeeDataService { get; set; }
+
+        public bool ShowDialog { get; set; }
+
+        public void Show()
+        {
+            ResetDialog();
+            ShowDialog = true;
+            StateHasChanged();
+        }
+
+        public void Close()
+        {
+            ShowDialog = false;
+            StateHasChanged();
+        }
+
+        private void ResetDialog()
+        {
+            Employee = new Employee
+            {
+                CountryId = 1,
+                JobCategoryId = 1,
+                BirthDate = DateTime.Now,
+                JoinedDate = DateTime.Now
+            };
+        }
+
+        protected async Task HandleValidSubmit()
+        {
+            await EmployeeDataService.AddEmployee(Employee).ConfigureAwait(false);
+            
+            ShowDialog = false;
+            StateHasChanged();
+
+        }
+
+
+    }
+}
--- BethanysPieShop.Server/Pages/EmployeeOverviewBase.cs
+++ BethanysPieShop.Server/Pages/EmployeeOverviewBase.cs
@@ -5,6 +5,7 @@
     using System.Linq;
     using System.Threading.Tasks;
     using BethanysPieShopHRM.Shared;
+    using Components;
     using Microsoft.AspNetCore.Components;
     using Services;
 
@@ -19,11 +20,19 @@
         [Inject]
         public IEmployeeDataService EmployeeDataService { get; set; }
 
+        // This property name HAS to match the @ref name in the template
+        protected AddEmployeeDialogBase AddEmployeeDialog { get; set; }
+
+
         protected override async Task OnInitializedAsync()
         {
             Employees = (await EmployeeDataService.GetAllEmployees().ConfigureAwait(false))?.ToList();
 
+        }
 
+        protected void QuickAddEmployee()
+        {
+            AddEmployeeDialog.Show();
         }
 
     }
